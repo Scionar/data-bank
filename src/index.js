810fb99f83209671666be173416d445ebdcfdc8f
Joona Viertola <contact@joonaviertola.com>
2019-06-14 19:31:15 +0300

    Server settings with hello world route

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const fastify = require('fastify')({
+  logger: true
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    const port = process.env.PORT || 3000;
+    await fastify.listen(port);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
