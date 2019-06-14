1eb3aa625096a43c34b77eccb2b2429e7761195f
Joona Viertola <contact@joonaviertola.com>
2019-06-14 20:24:03 +0300

    Separate server confgure code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-const fastify = require('fastify')({
-  logger: true
-});
-const mongoose = require('mongoose');
-
+const fastify = require('./server.js');
 const routes = require('./routes');
 const swagger = require('./config/swagger');
 
-const databaseHost = process.env.DATABASE_HOST || 'localhost';
-const databaseName = process.env.DATABASE_NAME || 'databank';
-
 fastify.register(require('fastify-swagger'), swagger.options);
-
-mongoose
-  .connect(`mongodb://${databaseHost}/${databaseName}`)
-  .then(() => console.log('MongoDB connected…'))
-  .catch(err => console.log(err));
-
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' };
-});
 
 routes.forEach((route, index) => {
   fastify.route(route);
@@ -26,8 +10,10 @@ routes.forEach((route, index) => {
 
 const start = async () => {
   try {
-    const port = process.env.PORT || 3000;
-    await fastify.listen(port);
+    const serverPort = process.env.SERVER_PORT || 3000;
+    const serverHost = process.env.SERVER_HOST || '0.0.0.0';
+
+    await fastify.listen(serverPort, serverHost);
     fastify.swagger();
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({
+  logger: true
+});
+const mongoose = require('mongoose');
+
+const databaseHost = process.env.DATABASE_HOST || 'localhost';
+const databaseName = process.env.DATABASE_NAME || 'databank';
+
+mongoose
+  .connect(`mongodb://${databaseHost}/${databaseName}`)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+module.exports = fastify;
