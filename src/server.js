3c1c767b0cf5c8b8b0ca61d2ffecb57e70e36c39
Joona Viertola <contact@joonaviertola.com>
2019-06-26 12:44:26 +0300

    Got rid off deprecation warning of MongoDB client URL

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ const databaseHost = process.env.DATABASE_HOST || 'localhost';
 const databaseName = process.env.DATABASE_NAME || 'databank';
 
 mongoose
-  .connect(`mongodb://${databaseHost}/${databaseName}`)
+  .connect(`mongodb://${databaseHost}/${databaseName}`, {
+    useNewUrlParser: true
+  })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
