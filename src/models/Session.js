8cae07e758a8b3db2e4bfe16877697b7fdee38f1
Joona Viertola <contact@joonaviertola.com>
2019-06-26 12:33:13 +0300

    Use mongoose specific Date type in Session model

--- src/models/Session.js
+++ src/models/Session.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
+const Date = mongoose.Schema.Types.Date;
 
 const sessionSchema = new mongoose.Schema({
   account_id: ObjectId,
